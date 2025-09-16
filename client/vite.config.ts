ecb0119020c6d1141d308c90ca041d76d85cc9d9
WilliamHoops2 <66770987+WilliamHoops2@users.noreply.github.com>
2025-09-16 14:22:08 +0800

    fixing 404 (7)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Project-Mikey/', 
+  plugins: [react()], 
+  // The base property is for GitHub Pages. You can remove it for Netlify.
+  base: '/', 
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:5000')
+  }
 })
